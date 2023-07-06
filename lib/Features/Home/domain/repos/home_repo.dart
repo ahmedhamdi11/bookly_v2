8b3cbde1a33f0394e9a12fee527506353ce503d5
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-07-07 00:55:25 +0300

    dartz added to use Either class from it

--- lib/Features/Home/domain/repos/home_repo.dart
+++ lib/Features/Home/domain/repos/home_repo.dart
@@ -1,6 +1,8 @@
 import 'package:bookly_v2/Features/Home/domain/entities/book_entity.dart';
+import 'package:bookly_v2/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<BookEntity> featchFeaturedBooks();
-  Future<BookEntity> featchNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> featchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> featchNewestBooks();
 }
