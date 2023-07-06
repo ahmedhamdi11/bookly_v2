f949ff99d9b2bd12cfbff2ddad3228d21c00ed8c
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-07-07 02:04:59 +0300

    create featch newest books use case

---
+++ lib/Features/Home/domain/use_cases/featch_newest_books_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:bookly_v2/Features/Home/domain/entities/book_entity.dart';
+import 'package:bookly_v2/Features/Home/domain/repos/home_repo.dart';
+import 'package:bookly_v2/core/errors/failure.dart';
+import 'package:bookly_v2/core/use_cases/use_case.dart';
+import 'package:dartz/dartz.dart';
+
+class FeatchNewestBooksUseCase extends UseCase<List<BookEntity>, void> {
+  final HomeRepo homeRepo;
+
+  FeatchNewestBooksUseCase(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> excute([void param]) async {
+    return await homeRepo.featchNewestBooks();
+  }
+}
