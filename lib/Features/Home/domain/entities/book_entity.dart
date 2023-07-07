44d5efe0d76967fd4b5b1241fbb072129e3b7ca6
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-07-08 00:20:20 +0300

    generate book entity type adapter

--- lib/Features/Home/domain/entities/book_entity.dart
+++ lib/Features/Home/domain/entities/book_entity.dart
@@ -1,8 +1,21 @@
+import 'package:hive/hive.dart';
+part 'book_entity.g.dart';
+
+@HiveType(typeId: 1)
 class BookEntity {
+  @HiveField(0)
   final String? image;
+
+  @HiveField(1)
   final String title;
+
+  @HiveField(2)
   final String? authorName;
+
+  @HiveField(3)
   final num? rating;
+
+  @HiveField(4)
   final num? rateCount;
 
   const BookEntity({
