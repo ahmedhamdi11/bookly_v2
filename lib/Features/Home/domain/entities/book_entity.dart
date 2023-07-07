a128a3f0562849a2d586767dd10990beeec933ed
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-07-07 22:42:18 +0300

    book model done

--- lib/Features/Home/domain/entities/book_entity.dart
+++ lib/Features/Home/domain/entities/book_entity.dart
@@ -1,10 +1,15 @@
 class BookEntity {
-  final String image;
+  final String? image;
   final String title;
-  final String authorName;
-  final num rating;
-  final num rateCount;
+  final String? authorName;
+  final num? rating;
+  final num? rateCount;
 
-  BookEntity(
-      this.image, this.title, this.authorName, this.rating, this.rateCount);
+  const BookEntity({
+    required this.image,
+    required this.title,
+    required this.authorName,
+    required this.rating,
+    required this.rateCount,
+  });
 }
